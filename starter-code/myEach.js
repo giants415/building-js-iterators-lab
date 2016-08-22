7b63f3ca6b4595da8786c2c143943a9e33bd34d4
Andrew <andrew.vinocur@gmail.com>
2016-08-22 11:57:33 -0700

    working myEach code

--- starter-code/myEach.js
+++ starter-code/myEach.js
@@ -1,14 +1,14 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/ForEach
 
 function myEach(arr, callback) {
-
-//  CODE INSIDE HERE   //
-
+  for (var i = 0; i < arr.length; i++) {
+    callback(arr[i], i, arr);
+  }
 }
 
 /*
  Best if you don't code out here.
- If you want to check your code, use tests or `index.js`!
+ If you w  to check your code, use tests or `index.js`!
 */
 
 
